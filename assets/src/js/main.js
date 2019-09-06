1344ec6642ceb5b8446976162445384b7e75c7ae
dmitriy76 <chirkov56@yandex.ru>
2019-09-06 12:41:08 +0700

    Тест

---
+++ assets/src/js/main.js
@@ -0,0 +1,14 @@
+// Импортируем jQuery
+//= ../../../node_modules/jquery/dist/jquery.js
+
+// Импортируем Popper
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+
+
+// Импортируем другие js-файлы
+//= slick.min.js
+//= my.js
---
+++ assets/src/js/my.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.single-item').slick({
+        'dots': true,
+        'arrows': false
+    });
+});
